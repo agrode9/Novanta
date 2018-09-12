e2a39edbe4ccfab616fb9a3ce16edff5bc44417d
agrode9 <agrode9@gmail.com>
2018-09-12 18:35:48 -0500

    preloader edits

--- scripts.js
+++ scripts.js
@@ -1,12 +1,42 @@
 $(window).load(function() {
-  window.setInterval(loadContent, 4000)
-});
+  if(window.location.href.match('/index') != null ||window.location.href.match('/index.html') != null)
+  {
+    window.setInterval(loadContent, 3000);
+  }
+  else if(window.location.href.match('/menu') != null ||window.location.href.match('/menu.html') != null)
+  {
+    document.getElementById("c3").style.display = "inline-block";
+    document.getElementById("c1").style.display = "inline-block";
 
+  }
+  else if(window.location.href.match('/photos') != null ||window.location.href.match('/photos.html') != null)
+  {
+    document.getElementById("c2").style.display = "inline-block";
+    document.getElementById("c1").style.display = "inline-block";
+
+  }
+  else if(window.location.href.match('/contact') != null ||window.location.href.match('/contact.html') != null)
+  {
+    document.getElementById("c4").style.display = "inline-block";
+    document.getElementById("c1").style.display = "inline-block";
+
+  }
+  else {
+    $('.preloader').fadeOut(0);
+    document.getElementById("c1").style.display = "inline-block";
+    document.getElementById("c2").style.display = "inline-block";
+    document.getElementById("c3").style.display = "inline-block";
+    document.getElementById("section1").style.display = "inline-block";
+    document.getElementById("c4").style.display = "inline-block";
+    document.getElementById("footer").style.display = "inline-block";
+  }
+});
 function loadContent(){
-  $('.preloader').fadeOut('slow');
-  document.getElementByClass("loader").style.display = "inline-block";
+    $('.preloader').fadeOut('slow');
+    document.getElementById("c1").style.display = "inline-block";
+    document.getElementById("c2").style.display = "inline-block";
+    document.getElementById("c3").style.display = "inline-block";
+    document.getElementById("section1").style.display = "inline-block";
+    document.getElementById("c4").style.display = "inline-block";
+    document.getElementById("footer").style.display = "inline-block";
 }
-
-$('.ml9 .letters').each(function(){
-  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-});
