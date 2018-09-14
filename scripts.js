f18525adaee64ff138f696b249e1a878208c54ad
Aaron Grode <grode2@wisc.edu>
2018-09-14 13:07:55 -0500

    test preloader

--- scripts.js
+++ scripts.js
@@ -2,25 +2,25 @@
 $(window).load(function() {
   if(window.location.href.match('/index') != null ||window.location.href.match('/index.html') != null)
   {
-    window.setInterval(loadContent, 2000);
+    window.setInterval(loadContent, 1000);
   }
   else if(window.location.href.match('/menu') != null ||window.location.href.match('/menu.html') != null)
   {
     document.getElementById("c3").style.display = "inline-block";
     document.getElementById("c1").style.display = "inline-block";
-
+    document.getElementById("footer").style.display = "inline-block";
   }
   else if(window.location.href.match('/photos') != null ||window.location.href.match('/photos.html') != null)
   {
     document.getElementById("c2").style.display = "inline-block";
     document.getElementById("c1").style.display = "inline-block";
-
+    document.getElementById("footer").style.display = "inline-block";
   }
   else if(window.location.href.match('/contact') != null ||window.location.href.match('/contact.html') != null)
   {
     document.getElementById("c4").style.display = "inline-block";
     document.getElementById("c1").style.display = "inline-block";
-
+    document.getElementById("footer").style.display = "inline-block";
   }
   else {
     $('.preloader').fadeOut(0);
